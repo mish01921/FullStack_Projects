9867fcdd5b69be7dd6d8955076058ba27e168cc7
mish01921 <misho191524@gmail.com>
2025-11-14 12:41:51 +0400

    i finished jest test

--- ch3/src/__tests__/posts.tests.js
+++ ch3/src/__tests__/posts.tests.js
@@ -1,7 +1,12 @@
 import mongoose from "mongoose"
-import { describe, expect, test } from "@jest/globals"
+import { describe, expect, test, beforeEach } from "@jest/globals"
 
-import { createPost } from "../services/posts"
+import {
+  createPost,
+  listAllPosts,
+  listPostsByAuthor,
+  listAllPostsByTag,
+} from "../services/posts"
 import { Post } from "../db/models/post"
 
 describe("creating posts", () => {
@@ -36,11 +41,72 @@ describe("creating posts", () => {
       expect(error.message).toContain("`title` is required")
     }
   })
+
   test("with minimal parameters should succeed", async () => {
     const post = {
       title: "Only a title",
     }
     const createdPost = await createPost(post)
     expect(createdPost._id).toBeInstanceOf(mongoose.Types.ObjectId)
+  })
+})
+
+const samplePosts = [
+  { title: "Leraning Redux", author: "Daniel Bugl", tags: ["redux"] },
+  { title: "Learn React Hooks", author: "Daniel Bugl", tags: ["react"] },
+  {
+    title: "Full-Stack React Projects",
+    author: "Daniel Bugl",
+    tags: ["react", "nodejs"],
+  },
+  { title: "Guide to TypeScript" },
+]
+
+let createdSamplePosts = []
+
+beforeEach(async () => {
+  await Post.deleteMany({})
+  createdSamplePosts = []
+  for (const post of samplePosts) {
+    const createdPost = new Post(post)
+    createdSamplePosts.push(await createdPost.save())
+  }
+})
+
+describe("listing posts", () => {
+  test("should return all posts ", async () => {
+    const posts = await listAllPosts()
+    expect(posts.length).toEqual(createdSamplePosts.length)
+  })
+  test("should return posts sorted by creaion date descending by default", async () => {
+    const posts = await listAllPosts()
+    const sortedSamplePosts = createdSamplePosts.sort(
+      (a, b) => b.createdAt - a.createdAt,
+    )
+    expect(posts.map((post) => post.createdAt)).toEqual(
+      sortedSamplePosts.map((post) => post.createdAt),
+    )
+  })
+
+  test("should take into account provided sorting optiions", async () => {
+    const posts = await listAllPosts({
+      sortBy: "updateAt",
+      sortOrder: "ascending",
+    })
+    const sortedSamplePosts = createdSamplePosts.sort(
+      (a, b) => a.updatedAt - b.updatedAt,
+    )
+    expect(posts.map((post) => post.updatedAt)).toEqual(
+      sortedSamplePosts.map((post) => post.updatedAt),
+    )
+  })
+  test("should be able to filter posts by author", async () => {
+    const posts = await listPostsByAuthor("Daniel Bugl")
+    expect(posts.length).toBe(3)
+  
+  })
+  test("should be able to filter posts by tag", async () => {
+    const posts = await listAllPostsByTag("nodejs")
+    expect(posts.length).toBe(1)
   })
 })
--- ch3/src/services/posts.js
+++ ch3/src/services/posts.js
@@ -1,7 +1,26 @@
 import { Post } from "../db/models/post.js"
 
-export async function createPost({title,author,contents,tags}) {
-    const post = new Post({title,author,contents,tags})
-    return  await post.save()
+export async function createPost({ title, author, contents, tags }) {
+  const post = new Post({ title, author, contents, tags })
+  return await post.save()
 }
 
+async function listsPosts(
+  query = {},
+  { sortBy = "createdAt", sortOrder = "descending" } = {},
+) {
+  const direction = sortOrder === "ascending" ? 1 : -1
+  return await Post.find(query).sort({ [sortBy]: direction })
+}
+
+export async function listAllPosts(options = {}) {
+  return await listsPosts({}, options)
+}
+
+export async function listPostsByAuthor(author, options = {}) {
+  return await listsPosts({ author }, options)
+}
+
+export async function listAllPostsByTag(tag, options = {}) {
+  return await listsPosts({ tags: tag }, options)
+}
